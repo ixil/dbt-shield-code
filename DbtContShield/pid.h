fe67e556628aa40e477b570c53b5482bf59592b4
Oliver Harley <oliver.r.harley@gmail.com>
2020-11-01 20:19:14 +0100

    PID fiddling

--- DbtContShield/pid.h
+++ DbtContShield/pid.h
@@ -2,12 +2,13 @@
 #include <AutoPID.h>
 
 #define SETPOINT_THRESHOLD 8
+#define CONTROL_THRESHOLD 4
 
 //pid settings and gains
 
-#define KP .12
-#define KI .0003
-#define KD 0
+#define KP 22.2
+#define KI 1.04
+#define KD 114
 #define PID_TIMEINTERVAL 1000
 #define OUTPUT_MIN  0
 #define OUTPUT_MAX  1
